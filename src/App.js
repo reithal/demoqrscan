f93730cc378f44c63fa5ab477c1f30ca6f019031
Carlos Mazon <57258681+reithal@users.noreply.github.com>
2020-06-10 17:59:18 -0400

    added qr scanner

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import Scanner from './components/Scanner'
 import './App.css';
 
 function App() {
@@ -16,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <Scanner />
         </a>
       </header>
     </div>
